b6c5025c45c1e71231f37ca2ed303e01e5aaace8
urban praper <urbanpraper@urbans-MacBook-Pro.local>
2026-07-18 19:46:25 +0200

    Day 1: Introduction exercises

---
+++ Day1/datatypes.py
@@ -0,0 +1,51 @@
+# This is the first comment
+#This is the second comment
+#Python is eating the world
+”””
+This is multiline comment
+multiline comment takes multiple lines.
+Python is eating the world
+
+
+#Numbers
+#integer is a data type that represents whole numbers. In Python, integers can be positive or negative and can be of any length.
+-3, -2, -1, 0, 1, 2, 3
+#float is a data type that represents real numbers. In Python, floats can be positive or negative and can be of any length.
+-3.14, -2.5, -1.0, 0.0, 1.0, 2.5, 3.14
+
+#string is a data type that is used to represent text. In Python, strings are enclosed in either single quotes or double quotes.
+'Urban'
+'Slovenia'
+'Python'
+'I love learning Python'
+'I am enjoying the first day of 30DaysOfPython Challenge'
+
+#boolenan is a data type that can only have two values: True or False. It is used to represent the truth value of an expression.
+True  # Is the light on? If it is on, then the value is True
+False # Is the light on? If it is off, then the value is False
+
+#list is a collection which is ordered and changeable. In Python lists are written with square brackets.
+[0, 1, 2, 3, 4, 5]  # all are the same data types - a list of numbers
+['Banana', 'Orange', 'Mango', 'Avocado'] # all the same data types - a list of strings (fruits)
+['Finland','Estonia', 'Sweden','Norway'] # all the same data types - a list of strings (countries)
+['Banana', 10, False, 9.81] # different data types in the list - string, integer, boolean and float
+
+#dictionary is a collection of key-value pairs. Each key is separated from its value by a colon (:), the items are separated by commas, and the whole thing is enclosed in curly braces {}. An empty dictionary without any items is written with just two curly braces, like this: {}.
+{
+'first_name':'Asabeneh',
+'last_name':'Yetayeh',
+'country':'Finland',
+'age':250,
+'is_married':True,
+'skills':['JS', 'React', 'Node', 'Python']
+}
+
+#tuple is a collection which is ordered and unchangeable. In Python tuples are written with round brackets.
+(0, 1, 2, 3, 4, 5) # Numbers
+('Asabeneh', 'Pawel', 'Brook', 'Abraham', 'Lidiya') # Names
+('Earth', 'Jupiter', 'Neptune', 'Mars', 'Venus', 'Saturn', 'Uranus', 'Mercury') # planets
+
+#set is a collection which is unordered and unindexed. In Python sets are written with curly brackets.
+{2, 4, 3, 5}
+{3.14, 9.81, 2.7} # order is not important in set
+”””
---
+++ Day1/exercises.py
@@ -0,0 +1,29 @@
+import math
+
+# Osnovne matematične operacije (Naloga 2 - Dan 1)
+print(3 + 4)    # seštevanje
+print(3 - 4)    # odštevanje
+print(3 * 4)    # množenje
+print(3 / 4)    # navadno deljenje
+print(3 % 4)    # ostanek pri deljenju
+print(3 ** 4)   # potenca
+print(3 // 4)   # celoštevilsko deljenje
+
+# Nizi (Naloga 3 - Dan 1)
+print("Urban")
+print("Praper")
+print("Slovenia")
+print("I am enjoying 30 days of python")
+
+# Tipi podatkov (Naloga 4 - Dan 1)
+print(type(10))
+print(type(9.8))
+print(type("Urban"))
+print(type(['Asabeneh', 'Python', 'Finland']))
+
+# Koren (kvadratni koren)
+print(math.sqrt(89))
+
+# Evklidska razdalja med (2, 3) in (10, 8)
+razdalja = math.sqrt((10 - 2) ** 2 + (8 - 3) ** 2)
+print(razdalja)
